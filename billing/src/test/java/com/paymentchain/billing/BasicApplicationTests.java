7ffb909746c16e2e5c102b47e10f8053b50e52eb
GitHubUrbanoReyser <106035896+GitHubUrbanoReyser@users.noreply.github.com>
2022-07-01 18:58:11 -0500

    Update BasicApplicationTests.java

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -7,7 +7,7 @@ import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class),,,,,
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
 
